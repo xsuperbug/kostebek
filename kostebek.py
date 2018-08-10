a42e43d080707e3e717b645642ea82133b121c38
Superbug <evren@superbug.co>
2018-08-10 15:52:14 +0300

    Update kostebek.py

--- kostebek.py
+++ kostebek.py
@@ -215,6 +215,9 @@ class kostebek:
 		chrome_options = Options()
 		chrome_options.add_argument("--window-size=1920x1080")
 		chrome_options.add_argument("--disable-infobars")
+		chrome_options.add_argument("--disable-dev-shm-usage") 
+		chrome_options.add_argument("disable-infobars")
+		chrome_options.add_argument("--no-sandbox")
 		chrome_driver = "/usr/local/bin/chromedriver"
 
 		driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
